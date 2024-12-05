857b47fbda658d1bc0b5c730451a9110e41299a8
Dexied <33525453+Dexied@users.noreply.github.com>
2024-12-06 03:45:28 +0500

    550-000

--- Source/XFoliageDemo.Target.cs
+++ Source/XFoliageDemo.Target.cs
@@ -8,7 +8,7 @@ public class XFoliageDemoTarget : TargetRules
 	public XFoliageDemoTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "XFoliageDemo" } );
 	}
