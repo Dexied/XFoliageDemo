a3db74a52be90bcbf34d745bfd86f137eec35917
Dexied <33525453+Dexied@users.noreply.github.com>
2026-01-15 01:31:12 +0500

    570-000

--- Source/XFoliageDemo.Target.cs
+++ Source/XFoliageDemo.Target.cs
@@ -8,8 +8,9 @@ public class XFoliageDemoTarget : TargetRules
 	public XFoliageDemoTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
-		ExtraModuleNames.AddRange( new string[] { "XFoliageDemo" } );
+        ExtraModuleNames.AddRange( new string[] { "XFoliageDemo" } );
 	}
 }
--- Source/XFoliageDemoEditor.Target.cs
+++ Source/XFoliageDemoEditor.Target.cs
@@ -8,8 +8,9 @@ public class XFoliageDemoEditorTarget : TargetRules
 	public XFoliageDemoEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 
-		ExtraModuleNames.AddRange( new string[] { "XFoliageDemo" } );
+        ExtraModuleNames.AddRange( new string[] { "XFoliageDemo" } );
 	}
 }
